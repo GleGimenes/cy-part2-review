c2b4f067e0de6e0f859afe04e1feed312c59cbd6
glegimenes <gle.gimenes@hotmail.com>
2024-01-29 16:43:08 -0300

    Login 3 otimizados os cenários de login com massa de dados, Fixture e Comandos Customizados

--- cypress/e2e/login/login3.cy.js
+++ cypress/e2e/login/login3.cy.js
@@ -3,39 +3,44 @@ const perfil = require('../../fixtures/perfil.json')
 
 describe('Funcionalidade Login', () => {
 
-    it('Deve fazer login com sucesso', () => {
+    beforeEach(() => {
         cy.visit('minha-conta/')
+    });
+
+    it('Deve fazer login com sucesso - Utilizando dados fixos', () => {
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
-
         cy.get('.page-title').should('contain', 'Minha conta')
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac (não é aluno_ebac? Sair)')
     })
 
-    it('Deve fazer login com sucesso - Utilizado massa de dados (fixture)', () => { //É criada uma vaiável (linha 2) que recebe o arquivo json.
-        cy.visit('minha-conta/')                                                    //
+    it('Deve fazer login com sucesso - Utilizado massa de dados (fixture)', () => { //É criada uma vaiável (linha 2) que recebe o arquivo json.                                                 //
         cy.get('#username').type(perfil.usuario)
         cy.get('#password').type(perfil.senha)
         cy.get('.woocommerce-form > .button').click()
-
         cy.get('.page-title').should('contain', 'Minha conta')
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac (não é aluno_ebac? Sair)')
 
     });
 
-    it.only('Deve fazer login com sucesso - Utilizado fixture (de forma nativa)', () => { //Não é necessário a importação, o método já é nativo do cypress
-        cy.visit('minha-conta/')                                                         //  Foi criada a variavel dados dentro do metodo
+    it('Deve fazer login com sucesso - Utilizado fixture (de forma nativa)', () => { //Não é necessário a importação, o método já é nativo do cypress                                                         //  Foi criada a variavel dados dentro do metodo
         cy.fixture('perfil').then(dados => {
             cy.get('#username').type(dados.usuario)
             cy.get('#password').type(dados.senha, { log: false })
             cy.get('.woocommerce-form > .button').click()
-
             cy.get('.page-title').should('contain', 'Minha conta')
             cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac (não é aluno_ebac? Sair)')
         })
     });
 
+    it('Deve fazer login com sucesso- Utilizando Comando Customizados', () => { //Não é necessário a importação, o método já é nativo do cypress                                                         //  Foi criada a variavel dados dentro do metodo
+        cy.login('aluno_ebac@teste.com', 'teste@teste.com')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac (não é aluno_ebac? Sair)')
+    });
 });
 
-// Funcionalidade Login utilizando massa de dados (fixture) e Fixture de forma nativa
+
+
+
+// Funcionalidade Login utilizando massa de dados (fixture), Fixture de forma nativa e Comando Customizado
