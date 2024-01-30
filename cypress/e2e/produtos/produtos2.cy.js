f1efe1cc232ea0e9c1ec1c6144ed2f1bc5dc8377
glegimenes <gle.gimenes@hotmail.com>
2024-01-30 16:22:11 -0300

    Implentando cenario de teste com massade de dados Fixture (json)

--- cypress/e2e/produtos/produtos2.cy.js
+++ cypress/e2e/produtos/produtos2.cy.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 import produtosPage from "../../support/page-objects/produtos.page";
+const produtos = require('../../fixtures/produtos.json')
 
 describe('Funcionalidade Página de produtos', () => {
 
@@ -15,18 +16,34 @@ it('Deve selecionar um produto da lista', () => {
 
 })
 
-it.only('Deve buscar um produto com sucesso', () => {
+it('Deve buscar um produto com sucesso', () => {
     produtosPage.buscarProduto('Beaumont Summit Kit')
     cy.get('.woocommerce-product-details__short-description > p').should('contain', 'This is a variable product called a Beaumont Summit Kit')
 
 });
 
 it('Deve visitar a página de um produto', () => {
-
+    produtosPage.visitarProduto('ariel-roll-sleeve-sweatshirt')
+    cy.get('.woocommerce-product-details__short-description > p').should('contain', 'This is a variable product called a Ariel Roll Sleeve Sweatshirt')
 });
 
 it('Deve adicionar produto ao carrinho', () => {
+    let quant = 3
+    produtosPage.buscarProdutoLista('Abominable Hoodie')
+    produtosPage.addProdutoCarrinho('M', 'Green', quant)
+    cy.get('.woocommerce-message').should('contain', quant + ' × “Abominable Hoodie” foram adicionados no seu carrinho.')
 
 });
 
+it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+    cy.fixture('produtos').then(dados => {
+        produtosPage.buscarProduto(dados[0].nomeProduto)
+        produtosPage.addProdutoCarrinho(dados[0].tamanho,
+            dados[0].cor,
+            dados[0].quantidade)
+        cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+    })
+});
+
 //Funcionalidade Página de produtos com cenário otimizado - Utilizando page objects
+//Utilizando massa de dados (fixture)
